22b00273ab1291007ad06acc36451cfd3a0bac03
Natalia Selvaggio <natalia.roselee@gmail.com>
2023-11-04 01:40:10 -0400

    put todo into another file.

--- team_5_app/lib/pages/student_dashboard/studentDashboard.dart
+++ team_5_app/lib/pages/student_dashboard/studentDashboard.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:team_5_app/pages/student_dashboard/incentives.dart';
 import 'package:team_5_app/pages/student_dashboard/stats.dart';
+import 'package:team_5_app/pages/student_dashboard/to_do.dart';
 
 class StudentDashboard extends StatelessWidget {
   const StudentDashboard({super.key});
@@ -55,39 +56,6 @@ class StudentDashboard extends StatelessWidget {
             Stats(),
             Incentives(),
           ]),
-        ));
-  }
-}
-
-class ToDo extends StatelessWidget {
-  String title;
-
-  ToDo({super.key, required this.title});
-
-  late Icon checkbox;
-  var isChecked = false.obs;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.only(bottom: 20),
-        child: ListTile(
-          onTap: () {
-            isChecked.value = !isChecked.value;
-          },
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          tileColor: Colors.white60,
-          leading: Obx(() => Icon(
-                isChecked.value
-                    ? Icons.check_box
-                    : Icons.check_box_outline_blank,
-                color: Colors.deepPurpleAccent,
-              )),
-          title: Text(title),
         ));
   }
 }
