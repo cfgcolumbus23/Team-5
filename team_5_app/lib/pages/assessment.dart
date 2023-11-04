ce71bed399f891a5e9730c0360d11975a8f949c4
Jay Baum <jaybaum.03@gmail.com>
2023-11-03 22:01:01 -0400

    created assessment page

---
+++ team_5_app/lib/pages/assessment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AssessmentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // assessment title
+
+    // assessment progress
+
+    // assessment grade
+  }
+}
