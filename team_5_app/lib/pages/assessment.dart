c59bdcdd3bf74dee64bd8f838db1639f9e5cb879
Natalia Selvaggio <natalia.roselee@gmail.com>
2023-11-03 23:23:15 -0400

    Created title header for individual assessment page.

--- team_5_app/lib/pages/assessment.dart
+++ team_5_app/lib/pages/assessment.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AssessmentPage extends StatelessWidget {
+  const AssessmentPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     // assessment title
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: const Text('AssessmentName Title Page',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 27,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
 
     // assessment progress
 
