c6f74178b0372fd7d603ef4fa1606ff81d3bd579
Akshar Jinandra <aksharjinandra@gmail.com>
2023-11-04 00:22:01 -0400

    added one graph

--- team_5_app/lib/pages/adminDashboard.dart
+++ team_5_app/lib/pages/adminDashboard.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class AdminPage extends StatefulWidget {
-  const AdminPage({super.key});
+  const AdminPage({Key? key}) : super(key: key);
   @override
-  State<AdminPage> createState() => _LoginPageState();
+  State<AdminPage> createState() => _AdminPageState();
 }
 
-class _LoginPageState extends State<AdminPage> {
-
+class _AdminPageState extends State<AdminPage> {
+  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: 3,
+      length: 2, // Corrected the length to 2 as you have only 2 tabs.
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Admin Dashboard'),
@@ -26,10 +27,44 @@ class _LoginPageState extends State<AdminPage> {
             ],
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
-            Center(
-              child: Text("Graphs here"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: LineChart(
+                LineChartData(
+                  gridData: FlGridData(show: false),
+                  titlesData: FlTitlesData(show: false),
+                  borderData: FlBorderData(
+                    show: true,
+                    border: Border.all(
+                      color: const Color(0xff37434d),
+                      width: 1,
+                    ),
+                  ),
+                  minX: 0,
+                  maxX: 7,
+                  minY: 0,
+                  maxY: 6,
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: [
+                        FlSpot(0, 3),
+                        FlSpot(1, 1),
+                        FlSpot(2, 4),
+                        FlSpot(3, 2.5),
+                        FlSpot(4, 1.5),
+                        FlSpot(5, 3),
+                        FlSpot(6, 3.5),
+                      ],
+                      isCurved: true,
+                      color: Colors.blue,
+                      dotData: FlDotData(show: false),
+                      belowBarData: BarAreaData(show: false),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Center(
               child: Text("Data here"),
