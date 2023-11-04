ed0382fc68b4ae54ca5df67774320d522193664a
bc6789 <bclemons6789@gmail.com>
2023-11-04 08:33:18 -0400

    connected to mongoDB, added registration endpoint, and updated login

--- api/index.js
+++ api/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const data = require("./data/data.js");
 const cors = require("cors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// mongoose.connect(process.env.MONGO_URL, (e) => {
+// 	throw e;
+// });
 app.use(cors());
 app.use(express.json());
+// const bcrypt = require("bcryptjs");
+// const User = require("./models/User.js");
+// const salt = bcrypt.genSaltSync(10);
 app.post("/login", (req, res) => {
 	//login endpoint for assigning roles
 	//returns a boolean based on id
@@ -13,8 +21,33 @@ app.post("/login", (req, res) => {
 	} else {
 		res.json({ admin: false }).status(200);
 	}
+	// const { username, password, id } = req.body;
+	// const userDoc = await User.findOne({ username });
+	// const passOk = bcrypt.compareSync(password, userDoc.password);
+	// if (passOk) {
+	// 	if (id == -1) {
+	// 		res.json({ admin: true }).status(200);
+	// 	}
+	// 	else{
+	// 		res.json({ admin: false }).status(200);
+	// 	}
+	// } else {
+	// 	res.status(400).json("wrong credentials");
+	// }
 });
 
+app.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	try {
+		const userDoc = await User.create({
+			username,
+			password: bcrypt.hashSync(password, salt),
+		});
+		res.json(userDoc);
+	} catch (e) {
+		res.status(400).json(e);
+	}
+});
 app.get("/assignments", (req, res) => {
 	//get all assignment data
 	/*/returns [{
