ee5ff930798e114e001bc10065678c3979761e47
Natalia Selvaggio <natalia.roselee@gmail.com>
2023-11-03 21:42:50 -0400

    Added to-do list in sidebar for Student Dashboard.

--- team_5_app/lib/pages/studentDashboard.dart
+++ team_5_app/lib/pages/studentDashboard.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class StudentDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Student HomePage'),
+      appBar: AppBar(
+        title: const Text('Student HomePage'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+                accountName: CircleAvatar(child: Text('JG')),
+                accountEmail: Column(
+                  children: [
+                    Text('UserName'),
+                    LinearProgressIndicator(
+                        value: 75.0, backgroundColor: Colors.amber),
+                  ],
+                )),
+            const ListTile(
+              title: Text('To-Do',
+                style: TextStyle(
+                  color: Color(0xFF755DC1),
+                  fontSize: 27,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w500,
+              ),
+              ),
+            ),
+            ToDo(),
+          ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: const [
-              UserAccountsDrawerHeader(
-                  accountName: CircleAvatar(child: Text('JG')),
-                  accountEmail: Column(
-                    children: [
-                      Text('email@example.com'),
-                      LinearProgressIndicator(
-                          value: 75.0, backgroundColor: Colors.amber),
-                    ],
-                  ))
-            ],
-          ),
-        ),
+      ), //
+    );
         body: const Center(
           child: Column(
             children: [
@@ -29,16 +42,42 @@ class StudentDashboard extends StatelessWidget {
               Incentives(),
             ]
           ),
-        ));
+        );
+  }
+}
+class ToDo extends StatelessWidget {
+   ToDo({super.key});
+
+    late Icon checkbox;
+    var isChecked = false.obs;
+
+  @override
+  Widget build(BuildContext context) {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: ListTile(
+          onTap: () {
+            isChecked.value = !isChecked.value;
+            },
+          shape: RoundedRectangleBorder (
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          tileColor: Colors.white60,
+          leading: Obx(()=>Icon(
+            isChecked.value?
+            Icons.check_box: Icons.check_box_outline_blank,
+            color: Colors.deepPurpleAccent,
+          )),
+          title: const Text('Complete certificate'),
+        )
+        );
   }
 }
 
 
-
 class Stats extends StatelessWidget{
   const Stats();
-  
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
